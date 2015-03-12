ca66da2a1bc801d0195779d67462f15f6606d313
Alan <alanstirling38@gmail.com>
2015-03-12 15:19:57 +0000

    More code changes
    This update parses the Title tags and content from the bulk HTML and
    places them in a HTML tag container. It then iterates through the
    contents and removes the HTML text leaving only the country names.

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using HtmlAgilityPack;
 
 namespace PaulsURLPageReader
 {
@@ -23,21 +24,56 @@ namespace PaulsURLPageReader
 
         private void btnGetPage_Click(object sender, EventArgs e)
         {
-            PageParse pp = new PageParse();
+            PageParser pp = new PageParser();
             
-            String pageAsString = pp.readPage(URL);
+            HtmlAgilityPack.HtmlDocument newDoc = pp.readPage(URL);
 
+            //Saves the HTML docuemnt as a single string for simple display in text box
+            String pageAsString = newDoc.DocumentNode.OuterHtml;
+            
             // save string to file
             File.WriteAllText(@"PageText.txt", pageAsString);
             
             // send to form
             Display(pageAsString);
+
+            //Now we have displayed the string we can work on parsing the text, we still have
+            //the newDoc in memory so we call this and it should return a list object of
+            //country names, without the HTML. 
+           
+            List<string> countryNames = new List<string>();
+            countryNames = pp.parseCountryName(newDoc);
+
+            Display(countryNames);
         }
         
+        //Original display method which will take a string parameter and display it in the top box
         void Display(string s)
         {
             tbURLText.Text = s;
         }
+
+        // Overlaod of display method which will take a List parameter and display in the bottom box
+        void Display(List<string> cleanedCountryNames)
+        {
+
+            String countryNames ="";
+            
+            //Create sting and newlines from country names
+            foreach (string item in cleanedCountryNames)
+            {
+                countryNames = countryNames + "\r\n" + item;
+
+            }
+
+            //Save to file
+            File.WriteAllText(@"ParsedPageText.txt", countryNames);
+
+            //display to bottom text box
+            tbCountryNames.Text = countryNames;
+        }
+
+
 
      
     }
---
+++ PageParser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+
+namespace PaulsURLPageReader
+{
+   class PageParser
+    {
+    
+        public HtmlDocument readPage(String url)
+        {
+                 
+            //New code, this fetches the page and saves the stream as a HTML document
+            HtmlWeb page = new HtmlWeb();
+            HtmlDocument newDoc = page.Load(url);
+
+           //Sends the HML page back
+            return newDoc;
+            
+        }
+
+        public List<string> parseCountryName(HtmlDocument doc)
+        {
+            //Parses out the country name using HTMLAgilityPack
+            //Apparently
+
+            List<string> countryNames = new List<string>();
+            List<string> cleanCountryNames = new List<string>();
+           
+            HtmlNodeCollection allNodes = doc.DocumentNode.SelectNodes("//td/b/a/@title");
+
+            if (allNodes != null)
+            {
+                //change all nodes into strings and then add them to 
+                //the list countrynames
+                foreach (HtmlNode thisNode in allNodes)
+                {
+                    //String s = thisNode.InnerText;
+                    String s = thisNode.WriteTo();
+                    countryNames.Add(s);
+                }
+
+                //quick parse throughthe string list to remove all the html crap
+
+                foreach (string item in countryNames)
+                {
+                    Regex tagRemove = new Regex(@"<.+?>");
+
+                    String cleanedItem;
+
+                    cleanedItem = tagRemove.Replace(item, string.Empty);
+                    cleanCountryNames.Add(cleanedItem);
+                    Console.Out.WriteLine(cleanedItem);
+
+                                     
+                }
+            }
+
+            return cleanCountryNames;
+
+
+        }
+
+      }
+
+
+}
